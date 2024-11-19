544d234180e556ee9c92c32f285f9cecee57f505
nes <nesb01t@163.com>
2024-11-19 18:22:10 +0800

    main lighting

--- components/showcase/index.vue
+++ components/showcase/index.vue
@@ -11,18 +11,18 @@ const obj = await modelBridge.getGltfExample();
     <TresCanvas clear-color="#080808" preset="realistic">
       <TresPerspectiveCamera :position="[3, 3, 3]" />
       <OrbitControls make-default />
+      <TresGridHelper :size="10" :divisions="10" />
 
       <!-- items -->
       <primitive :object="obj" />
 
       <!-- env -->
       <Stars :rotation="[0, 0, 0]" :radius="50" :depth="50" :count="2000" :size="0.3" :size-attenuation="true" />
-      <TresAmbientLight :intensity="0.18" />
-      <TresDirectionalLight shadow-bias="-0.0001" cast-shadow :position="[0, 0, 2]" :intensity="0.35" />
-      <TresDirectionalLight shadow-bias="-0.0001" cast-shadow :position="[0, 2, 0]" :intensity="0.45" />
-      <TresDirectionalLight shadow-bias="-0.0001" cast-shadow :position="[-2, 0, 0]" :intensity="0.5" />
-      <TresDirectionalLight shadow-bias="-0.0001" cast-shadow :position="[2, 0, 0]" :intensity="0.1" />
-      <TresGridHelper :size="10" :divisions="10" />
+      <TresAmbientLight :intensity="0.2" />
+
+      <!-- light & shadow -->
+      <TresDirectionalLight shadow-bias="-0.0005" cast-shadow :position="[-3, 4, 0]" :intensity="0.35" />
+      <TresDirectionalLight shadow-bias="-0.0005" cast-shadow :position="[1, 2, -0.5]" :intensity="0.7" />
     </TresCanvas>
 
     <ShowcaseHelper />
